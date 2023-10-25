c383688f933f1a6ca443a30acdea13532d79433a
Deivid Souza <81912820+deivid-souza95@users.noreply.github.com>
2023-10-25 20:46:05 -0300

    Delete focustimer2.0 directory

--- focustimer2.0/src/FocusTimer2.0/elements.js
+++
@@ -1,5 +0,0 @@
-export const controls = document.getElementById('controls') // acao para registrar o click do botao, lembrando que ele esta pegando o ID do HTML
-
-export const minutes = document.getElementById('minutes')
-
-export const seconds = document.getElementById('seconds')
--- focustimer2.0/src/main.js
+++
@@ -1,3 +0,0 @@
-import * as FocusTimer from "./FocusTimer2.0/index.js"
-
-FocusTimer.start(0,3)
